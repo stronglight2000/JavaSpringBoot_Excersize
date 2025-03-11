6da75cff3bece2a8d326b90c4a2613ea0a8f8094
stronglight2000 <stronglight2000@gmail.com>
2025-03-12 00:57:47 +0700

    btb1

---
+++ btb1/src/main/java/com/example/bt1/model/Product.java
@@ -0,0 +1,19 @@
+package com.example.bt1.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+    private String id;
+    private String name;
+    private String description;
+    private int price;
+    private String brand;
+
+}
+
